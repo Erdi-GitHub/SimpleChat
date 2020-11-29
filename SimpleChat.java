b005f978244d2f7a6ff06c3fe8222d3b92b2be24
Erdi-GitHub <75197158+Erdi-GitHub@users.noreply.github.com>
2020-11-29 08:13:59 +0100

    Added SimpleChat main Class to main branch

---
+++ SimpleChat.java
@@ -0,0 +1,85 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package main;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleChat extends JavaPlugin implements Listener
+{
+    private volatile boolean chatEnabled;
+    
+    public SimpleChat() {
+        this.chatEnabled = true;
+    }
+    
+    @EventHandler
+    public void onAsyncPlayerChat(final AsyncPlayerChatEvent event) {
+        if (!this.chatEnabled && !event.getPlayer().hasPermission("simplechat.bypassmutechat")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(this.color("&4&lChat is muted."));
+        }
+    }
+    
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents((Listener)this, (Plugin)this);
+        System.out.println("[SimpleChat] Enabled.\nPlease support Erdi__ in any way you can if you do use this plugin.\nJust using it is greatly appreciated. -Erdi__");
+        this.saveDefaultConfig();
+    }
+    
+    public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
+        if (cmd.getName().equalsIgnoreCase("clearchat")) {
+            if (sender.hasPermission("simplechat.clearchat")) {
+                Bukkit.broadcastMessage(this.color("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n  \n \n "));
+                if (this.getConfig().getBoolean("clearchatsender")) {
+                    Bukkit.broadcastMessage(this.color("&7Chat cleared by &b" + sender.getName() + "."));
+                }
+                else {
+                    Bukkit.broadcastMessage(this.color("&7Chat has been cleared."));
+                }
+            }
+            else {
+                Bukkit.broadcastMessage(this.getConfig().getString(this.color("unknowncommand")));
+            }
+        }
+        if (cmd.getName().equalsIgnoreCase("mutechat")) {
+            if (sender.hasPermission("simplechat.mutechat")) {
+                this.chatEnabled = !this.chatEnabled;
+                sender.sendMessage(this.color(this.chatEnabled ? "&bUnmuted the chat" : "&bMuted the chat"));
+                if (!this.chatEnabled) {
+                    if (this.getConfig().getBoolean("mutechatsender")) {
+                        Bukkit.broadcastMessage(this.color("&4&lChat has been muted by &b" + sender.getName()));
+                    }
+                    else {
+                        Bukkit.broadcastMessage(this.color("&4&lChat has been muted."));
+                    }
+                }
+                else if (this.chatEnabled) {
+                    if (this.getConfig().getBoolean("mutechatsender")) {
+                        Bukkit.broadcastMessage(this.color("&4&lChat has been unmuted by &b" + sender.getName()));
+                    }
+                    else {
+                        Bukkit.broadcastMessage(this.color("&4&lChat has been unmuted."));
+                    }
+                }
+            }
+            else {
+                Bukkit.broadcastMessage(this.getConfig().getString(this.color("unknowncommand")));
+            }
+        }
+        return false;
+    }
+    
+    public String color(final String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
